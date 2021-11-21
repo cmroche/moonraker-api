178a8eee89ff2ca9143e301d929db1970bedfc7d
Clifford Roche <clifford.roche@gmail.com>
2021-11-20 20:53:21 -0500

    Prep for release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="moonraker-api",
-    version="0.0.1",
+    version="0.1.0",
     python_requires=">=3.8",
     install_requires=requirements,
     author="Clifford Roche",
